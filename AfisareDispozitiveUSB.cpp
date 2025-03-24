c52b23c16596b2040f50d6c85f531731c0e38670
madalina132 <149997251+madalina132@users.noreply.github.com>
2025-03-24 19:45:29 +0200

    Add files via upload

---
+++ AfisareDispozitiveUSB.cpp
@@ -0,0 +1,28 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+/*
+int main() {
+    FILE* fp;
+    char buffer[256];
+
+    printf("Dispozitive USB conectate:\n\n");
+
+    // Executăm comanda 'pnputil /enum-devices /class USB' și capturăm output-ul
+    fp = _popen("pnputil /enum-devices /class USB", "r");
+    if (fp == NULL) {
+        printf("Eroare la executarea comenzii.\n");
+        return 1;
+    }
+
+    // Citim și afișăm linie cu linie rezultatul
+    while (fgets(buffer, sizeof(buffer), fp) != NULL) {
+        printf("%s", buffer);
+    }
+
+    _pclose(fp);
+
+    printf("\nApasati Enter pentru a inchide...");
+    getchar();
+    return 0;
+}*/
---
+++ memorii.cpp
@@ -0,0 +1,45 @@
+﻿#include <stdio.h>
+#include <windows.h>
+
+int main() {
+    SYSTEM_INFO sysInfo;
+    DWORD pageSize;
+    LPVOID memoria = NULL;
+    // Număr fix de pagini - modifică aici pentru a aloca mai multe sau mai puține
+    int numarPagini = 10;
+
+    // Aflăm dimensiunea unei pagini
+    GetSystemInfo(&sysInfo);
+    pageSize = sysInfo.dwPageSize;
+
+    printf("Dimensiunea unei pagini: %lu bytes\n", pageSize);
+    printf("Se aloca %d pagini...\n", numarPagini);
+
+    // Alocăm memoria
+    SIZE_T dimensiuneTotala = (SIZE_T)pageSize * numarPagini;
+
+    memoria = VirtualAlloc(NULL, dimensiuneTotala, MEM_COMMIT, PAGE_READWRITE);
+
+    if (memoria == NULL) {
+        printf("Eroare la alocare! Cod: %lu\n", GetLastError());
+        return 1;
+    }
+
+    printf("Memorie alocata la adresa: 0x%p\n", memoria);
+    printf("Total memorie alocata: %llu bytes\n", (unsigned long long)dimensiuneTotala);
+
+    // Scriem ceva în memorie pentru test
+    for (int i = 0; i < numarPagini; i++) {
+        char* adrPagina = (char*)memoria + (i * pageSize);
+        *adrPagina = (char)i;
+    }
+
+    // Eliberăm memoria
+    VirtualFree(memoria, 0, MEM_RELEASE);
+    printf("Memoria a fost eliberata\n");
+
+    printf("\nApasa Enter pentru a inchide...");
+    getchar();
+
+    return 0;
+}
